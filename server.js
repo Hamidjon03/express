fe17b4af56c19188895eac41e6da2ab11230d782
Hamidjon03 <forevalm@gmail.com>
2023-02-24 17:52:40 +0500

    les2 connect to localhost DB

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const dotenv = require('dotenv')
 const {engine} = require('express-handlebars')
+const mongoose = require('mongoose')
 
 
 // Config DOTENV
@@ -23,6 +24,16 @@ app.set('view engine', '.hbs')
 // Initialize Routes
 app.use('/', require('./routes/homeRoutes'))
 app.use('/posters', require('./routes/posterRoutes'))
+
+// connect to DB
+const connectDB = async () => {
+  mongoose.set("strictQuery", false);
+  const connecting = await mongoose.connect('mongodb://localhost:27017/postersapp')
+  console.log(`MongoDB connected to: ${connecting.connection.host}`)
+
+}
+
+connectDB()
 
 const PORT = process.env.PORT || 5000
 
