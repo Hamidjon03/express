7b4192d95319f61ef2a6ee4accb7ccbad3970bdd
Hamidjon03 <forevalm@gmail.com>
2023-03-06 16:49:38 +0500

    auth less7 Bazaga connection ulaymiz

--- controllers/posterControllers.js
+++ controllers/posterControllers.js
@@ -1,5 +1,5 @@
 const Poster = require('../models/posterModel')
-const { v4 } = require('uuid')
+const User = require("../models/userModel")
 
 
 //@route       GET /posters
@@ -36,15 +36,24 @@ const addNewPosterPage = (req, res) => {
 //@access      Private
 const addNewPoster = async (req, res) => {
   try {
-    const poster = {
+    const newPoster = new Poster({
       title: req.body.title,
       amount: req.body.amount,
       region: req.body.region,
       description: req.body.description,
       image: 'uploads/' + req.file.filename
-    }
-    await Poster.create(poster)
-    res.redirect('/posters')
+    })
+
+    await User.findByIdAndUpdate(req.session.user._id,
+      { $push: { posters: newPoster._id } },
+      { new: true, upsert: true })
+
+    await newPoster.save((err, posterSaved) => {
+      if (err) throw err
+      const posterId = posterSaved._id
+      res.redirect("/posters/" + posterId)
+    })
+
   } catch (err) {
     console.log(err)
   }
@@ -57,10 +66,11 @@ const addNewPoster = async (req, res) => {
 //@access      Public 
 const getOnePoster = async (req, res) => {
   try {
-    const poster = await Poster.findByIdAndUpdate(req.params.id, {$inc: {visits: 1}}, {new: true}).lean()
+    const poster = await Poster.findByIdAndUpdate(req.params.id, { $inc: { visits: 1 } }, { new: true }).lean()
     res.render("poster/one", {
       title: poster.title,
       url: process.env.URL,
+      user: req.session.user,
       poster
     })
   } catch (err) {
--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new Schema({
     required: true,
     minLength: 6
   },
+  posters: [{ type: Schema.Types.ObjectId, ref: 'Poster' }]
 }, {
   timestamps: true
 })
